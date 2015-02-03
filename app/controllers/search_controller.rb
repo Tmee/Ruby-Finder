a63d3b28acc5cb80a7538edee10771c4a76561a2
Timothy Mee <tjmee90@gmail.com>
2015-02-03 14:29:35 -0700

    added indeed to search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,18 @@
 class SearchController < ApplicationController
 
   def create
-    get_html_doc(params[:city], params[:state])
-    @jobs = scrape_for_jobs
+    get_html_docs(params[:city], params[:state])
+    @monster_jobs = scrape_for_monster_jobs
+    @indeed_jobs  = scrape_for_indeed_jobs
   end
 
-  def get_html_doc(city, state)
-    @doc  = Nokogiri::HTML(open("http://jobsearch.monster.com/search/ruby_5?where=#{city}__2C-#{state}"))
+  def get_html_docs(city, state)
+    @monster_doc = Nokogiri::HTML(open("http://jobsearch.monster.com/search/ruby_5?where=#{city}__2C-#{state}"))
+    @indeed_doc  = Nokogiri::HTML(open("http://www.indeed.com/jobs?q=ruby&l=#{city}%2C+#{state}"))
   end
 
-  def scrape_for_jobs
-    rows = @doc.xpath("//div[contains(@class, 'leftContainer')]//div[contains(@id, 'primaryResults')]//table//tr[position() > 1]//td[position() = 2]//div[contains(@class, 'jobTitleContainer')]//a")
+  def scrape_for_monster_jobs
+    rows = @monster_doc.xpath("//div[contains(@class, 'leftContainer')]//div[contains(@id, 'primaryResults')]//table//tr[position() > 1]//td[position() = 2]//div[contains(@class, 'jobTitleContainer')]//a")
     jobs_array = []
     rows.collect do |row|
       detail = {}
@@ -20,11 +22,16 @@ class SearchController < ApplicationController
       end
     jobs_array
   end
+
+   def scrape_for_indeed_jobs
+    rows = @indeed_doc.xpath("//table//tr//td//table//tr//td[contains(@id ,'resultsCol')]//div[contains(@class, 'row  result')]//h2//a")
+    jobs_array = []
+    rows.collect do |row|
+      detail = {}
+      detail[:title] = row.text.gsub(/\s{3}/, '')
+      detail[:link]  = "www.indeed.com#{row.attribute('href').value}"
+      jobs_array << detail
+      end
+    jobs_array
+  end
 end
-
-
-
-
-
-    # titles = @doc.xpath("//div[contains(@class, 'leftContainer')]//div[contains(@id, 'primaryResults')]//table//tr[position() > 1]//td[position() = 2]//div[contains(@class, 'jobTitleContainer')]//a").map {|x| x.text.gsub(/\s{3}/, '')}
-    # links  = @doc.xpath("//div[contains(@class, 'leftContainer')]//div[contains(@id, 'primaryResults')]//table//tr[position() > 1]//td[position() = 2]//div[contains(@class, 'jobTitleContainer')]//a").map {|x|x.attribute('href').value}
