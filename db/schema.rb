0b6d01c4bd35a48fc4d2c0718e46d63ac1248eec
Timothy Mee <tjmee90@gmail.com>
2015-02-03 13:34:19 -0700

    removed search model stuff from first thoughts

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203180425) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "search", force: true do |t|
-    t.string   "city"
-    t.string   "state"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
