6828e5390e0b04ce32d79516c3c7b37618c04675
Timothy Mee <tjmee90@gmail.com>
2015-02-04 13:42:54 -0700

    refactored controller, pushed all sraping into search model.  Model still needs refactoring

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,64 +1,27 @@
 class SearchController < ApplicationController
 
   def create
-    if params[:city].present? || params[:state].present?
-      city  = params[:city].gsub(/\s{1}/, '-')
-      state = params[:state].gsub(/\s{1}/, '-')
-      get_html_docs(city, state)
-      scrape_for_data
+    if search_present?
+      format_search
+      Search.get_html_docs(@city, @state)
+      scrape_for_jobs
     end
   end
-
-  def get_html_docs(city, state = '')
-    @monster_doc = Nokogiri::HTML(open("http://jobsearch.monster.com/search/ruby_5?where=#{city}__2C-#{state}"))
-    @indeed_doc  = Nokogiri::HTML(open("http://www.indeed.com/jobs?q=ruby&l=#{city}%2C+#{state}"))
-    if city.empty?
-      begin
-        @simply_doc  = Nokogiri::HTML(open("http://www.simplyhired.com/search?q=ruby&l=#{state}"))
-        rescue OpenURI::HTTPError
-      end
-    else
-      begin
-        @simply_doc  = Nokogiri::HTML(open("http://www.simplyhired.com/search?q=ruby&l=#{city}%2C+#{state}"))
-        rescue OpenURI::HTTPError
-      end
-    end
-  end
-
-  def scrape_for_monster_jobs
-    rows = @monster_doc.xpath("//div[contains(@class, 'leftContainer')]//div[contains(@id, 'primaryResults')]//table//tr[position() > 1]//td[position() = 2]//div[contains(@class, 'jobTitleContainer')]//a")
-    collect_data(rows)
-  end
-
-  def scrape_for_indeed_jobs
-    rows = @indeed_doc.xpath("//table//tr//td//table//tr//td[contains(@id ,'resultsCol')]//div[contains(@class, 'row  result')]//h2//a")
-    collect_data(rows, "www.indeed.com")
-  end
-
-  def scrape_for_simplyhired_jobs
-    if @simply_doc.present?
-      rows = @simply_doc.xpath("//div[contains(@id, 'content')]//div[contains(@id, 'search_results')]//div[contains(@class, 'column_center_inner')]//div[contains(@class, 'results')]//ul//li//div[position() = 1]//h2//a")
-      collect_data(rows, "www.simplyhired.com")
-    end
-  end
-
 
   private
 
-  def scrape_for_data
-    @monster_jobs     = scrape_for_monster_jobs
-    @indeed_jobs      = scrape_for_indeed_jobs
-    @simplyhired_jobs = scrape_for_simplyhired_jobs
+  def search_present?
+    params[:city].present? || params[:state].present?
   end
 
-  def collect_data(rows, url = nil)
-    jobs_array = []
-    rows.collect do |row|
-      detail = {}
-      detail[:title] = row.text.gsub(/\s{3}/, '')
-      detail[:link]  = "#{url}#{row.attribute('href').value}"
-      jobs_array << detail
-      end
-    jobs_array
+  def format_search
+    @city  = params[:city].gsub(/\s{1}/, '-')
+    @state = params[:state].gsub(/\s{1}/, '-')
+  end
+
+  def scrape_for_jobs
+    @monster_jobs     = Search.monster_jobs
+    @indeed_jobs      = Search.indeed_jobs
+    @simplyhired_jobs = Search.simplyhired_jobs
   end
 end
