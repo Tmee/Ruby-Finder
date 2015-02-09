481d7b1532e209b0385a64d6eb6b2ce457c281ae
Timothy Mee <tjmee90@gmail.com>
2015-02-09 10:14:49 -0700

    moved search result conditions into appliction helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def monster_jobs?
+    @monster_jobs.present?
+  end
+
+  def indeed_jobs?
+    @indeed_jobs.present?
+  end
+
+  def simplyhired_jobs?
+    @simplyhired_jobs.present?
+  end
 end
