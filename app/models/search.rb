f35507d7199903782dda62ef399c7126e378ee7b
Timothy Mee <tjmee90@gmail.com>
2015-02-23 15:48:31 -0700

    refactored model, removed simply hired exceptions for no city, and just require both city and state be entered to serach

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,24 +1,14 @@
 class Search < ActiveRecord::Base
 
   def self.get_html_docs(city, state = '')
-    @monster_doc = Nokogiri::HTML(open("http://jobsearch.monster.com/search/ruby_5?where=#{city}__2C-#{state}"))
-    @indeed_doc  = Nokogiri::HTML(open("http://www.indeed.com/jobs?q=ruby&l=#{city}%2C+#{state}"))
     @dice_doc    = Nokogiri::HTML(open("https://www.dice.com/jobs?q=ruby&l=#{city}%2C+#{state}"))
-    if city.empty?
-      begin
-        @simply_doc  = Nokogiri::HTML(open("http://www.simplyhired.com/search?q=ruby&l=#{state}"))
-        rescue OpenURI::HTTPError
-      end
-    else
-      begin
-        @simply_doc  = Nokogiri::HTML(open("http://www.simplyhired.com/search?q=ruby&l=#{city}%2C+#{state}"))
-        rescue OpenURI::HTTPError
-      end
-    end
+    @indeed_doc  = Nokogiri::HTML(open("http://www.indeed.com/jobs?q=ruby&l=#{city}%2C+#{state}"))
+    @simply_doc  = Nokogiri::HTML(open("http://www.simplyhired.com/search?q=ruby&l=#{city}%2C+#{state}"))
+    @monster_doc = Nokogiri::HTML(open("http://jobsearch.monster.com/search/ruby_5?where=#{city}__2C-#{state}"))
   end
 
-  def self.monster_jobs
-    rows = @monster_doc.xpath("//div[contains(@class, 'leftContainer')]//div[contains(@id, 'primaryResults')]//table//tr[position() > 1]//td[position() = 2]//div[contains(@class, 'jobTitleContainer')]//a")
+  def self.dice_jobs
+    rows = @dice_doc.xpath("//div[contains(@id, 'resultSec')]//div[contains(@class, 'serp-result-content')]//a")
     collect_data(rows)
   end
 
@@ -27,16 +17,14 @@ class Search < ActiveRecord::Base
     collect_data(rows, "www.indeed.com")
   end
 
-  def self.dice_jobs
-    rows = @dice_doc.xpath("//div[contains(@id, 'resultSec')]//div[contains(@class, 'serp-result-content')]//a")
-    collect_data(rows)
+  def self.simplyhired_jobs
+    rows = @simply_doc.xpath("//div[contains(@id, 'content')]//div[contains(@id, 'search_results')]//div[contains(@class, 'column_center_inner')]//div[contains(@class, 'results')]//ul//li//div[position() = 1]//h2//a")
+    collect_data(rows, "www.simplyhired.com")
   end
 
-  def self.simplyhired_jobs
-    if @simply_doc.present?
-      rows = @simply_doc.xpath("//div[contains(@id, 'content')]//div[contains(@id, 'search_results')]//div[contains(@class, 'column_center_inner')]//div[contains(@class, 'results')]//ul//li//div[position() = 1]//h2//a")
-      collect_data(rows, "www.simplyhired.com")
-    end
+  def self.monster_jobs
+    rows = @monster_doc.xpath("//div[contains(@class, 'leftContainer')]//div[contains(@id, 'primaryResults')]//table//tr[position() > 1]//td[position() = 2]//div[contains(@class, 'jobTitleContainer')]//a")
+    collect_data(rows)
   end
 
   def self.collect_data(rows, url = nil)
