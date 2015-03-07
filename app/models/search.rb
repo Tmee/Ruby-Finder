e7a154bebed906806182081937cf0666bc47c5eb
Timothy Mee <tjmee90@gmail.com>
2015-03-06 22:24:35 -0700

    added indeed model, and company names

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,7 +4,6 @@ class Search < ActiveRecord::Base
     @dice_doc    = Nokogiri::HTML(open("https://www.dice.com/jobs?q=ruby&l=#{city}%2C+#{state}"))
     @indeed_doc  = Nokogiri::HTML(open("http://www.indeed.com/jobs?q=ruby&l=#{city}%2C+#{state}"))
     @simply_doc  = Nokogiri::HTML(open("http://www.simplyhired.com/search?q=ruby&l=#{city}%2C+#{state}"))
-    @monster_doc = Nokogiri::HTML(open("http://jobsearch.monster.com/search/ruby_5?where=#{city}__2C-#{state}"))
   end
 
   def self.dice_jobs
@@ -20,11 +19,6 @@ class Search < ActiveRecord::Base
   def self.simplyhired_jobs
     rows = @simply_doc.xpath("//div[contains(@id, 'content')]//div[contains(@id, 'search_results')]//div[contains(@class, 'column_center_inner')]//div[contains(@class, 'results')]//ul//li//div[position() = 1]//h2//a")
     collect_data(rows, "www.simplyhired.com")
-  end
-
-  def self.monster_jobs
-    rows = @monster_doc.xpath("//div[contains(@class, 'leftContainer')]//div[contains(@id, 'primaryResults')]//table//tr[position() > 1]//td[position() = 2]")
-    collect_data(rows)
   end
 
   def self.collect_data(rows, url = nil)
