6ed4dd6c09c6547bfc18e6b3e536ffe1981d6a65
Timothy Mee <tjmee90@gmail.com>
2015-03-07 21:19:17 -0700

    dice modal made and working

---
+++ app/models/dice.rb
@@ -0,0 +1,21 @@
+class Dice
+
+  def self.get_document(city, state)
+    @doc = Nokogiri::HTML(open("https://www.dice.com/jobs?q=ruby&l=#{city}%2C+#{state}"))
+  end
+
+  def self.jobs
+    rows = @doc.xpath("//div[contains(@id, 'resultSec')]//div[contains(@class, 'serp-result-content')]")
+    collect_data(rows)
+  end
+
+  def self.collect_data(rows)
+    rows.collect do |row|
+        {
+        :title => row.xpath("h3//a").text.gsub(/\s{3}/, ''),
+        :link  => "#{row.xpath("h3//a").attribute('href').value}",
+        :company_name => row.xpath("ul//li[contains(@class, 'employer')]//a").text
+      }
+    end
+  end
+end
