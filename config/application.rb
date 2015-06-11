0c34f5c89e8709e7b51492e3494b44c403127aef
Timothy Mee <tjmee90@gmail.com>
2015-06-11 14:20:22 -0600

    switched old db name to ruby finder

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RubyMonster
+module RubyFinder
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join("app", "assets", "stylesheets", "foundation-icons")
+    config.assets.paths << Rails.root.join("app", "assets", "stylesheets")
   end
 end
