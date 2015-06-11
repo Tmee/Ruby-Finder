10274490d9c4163d2831a3d880b3df951a960eb8
Timothy Mee <tjmee90@gmail.com>
2015-06-11 15:47:52 -0600

    added login path and button to login on the home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'search#create'
 
   get "/auth/twitter/callback", to: "sessions#create"
+  get "/auth/twitter", as: "login"
   delete '/logout', to: "sessions#destroy"
   resources :users
 
