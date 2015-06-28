72624457d9efc2a47e210f77d5fd409c296e6949
Timothy Mee <tjmee90@gmail.com>
2015-06-28 12:12:48 -0600

    get request sending an increasing number of requests back. problem.

--- app/assets/javascripts/notePad/setSelectedNote.js
+++ app/assets/javascripts/notePad/setSelectedNote.js
@@ -1,18 +1,42 @@
-$(document).ready(function() {
-  setInterval(function(){
-    $("li.note-title").click(function() {
-      console.log("hit ::::::::::::::::::::::");
-      $('textarea#selected_note').attr('data-note-id', $(this).data('note-id'))
-      setSelectedNote($(this).data());
-    });
-  }, 1000);
+var t;
+var clickedTitle = function() {
+  console.log("loaded ::::::::::::")
+  $("li.note-title").click(function() {
+    console.log("clicked ::::::::::::")
+    $('textarea#selected_note').attr('data-note-id', $(this).data('note-id'))
+    setSelectedNote($(this).data());
+    ///t = setTimeout(function(){ clickedTitle() }, 1000);
+  });
 
-  var setSelectedNote = function(data) {
-    var id = data.noteId;
-    var url = "/notes/" + id;
-    $.get(url, function(data){
-      $('input#selected_note_title').val(data.title);
-      $('textarea#selected_note').val(data.body);
-    });
-  };
-});
+};
+
+var setSelectedNote = function(data) {
+  ///clearTimeout(t);
+  var id = data.noteId;
+  var url = "/notes/" + id;
+  $.get(url, function(data){
+    $('input#selected_note_title').val(data.title);
+    $('textarea#selected_note').val(data.body);
+    clickedTitle();
+  });
+};
+
+window.onload = clickedTitle;
+
+
+
+
+
+
+
+
+// var interval = window.onload = setInterval(function() {
+//   console.log("loaded ::::::::::::::::::::");
+//   $("li.note-title").click(function() {
+//     window.clearInterval(interval);
+//     console.log("hit ::::::::::::::::::::::");
+//     $('textarea#selected_note').attr('data-note-id', $(this).data('note-id'))
+//     setSelectedNote($(this).data());
+//     interval;
+//   });
+// }, 1000);
