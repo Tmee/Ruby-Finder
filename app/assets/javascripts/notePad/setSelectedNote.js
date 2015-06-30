005d7cc28f0e734a4502dba58260f1ad16486d7f
Timothy Mee <tjmee90@gmail.com>
2015-06-30 14:08:26 -0600

    load, click, reload function of the title list and selected note working

--- app/assets/javascripts/notePad/setSelectedNote.js
+++ app/assets/javascripts/notePad/setSelectedNote.js
@@ -1,17 +1,16 @@
-var t;
 var clickedTitle = function() {
   console.log("loaded ::::::::::::")
+  $("li.note-title").unbind('click');
   $("li.note-title").click(function() {
     console.log("clicked ::::::::::::")
     $('textarea#selected_note').attr('data-note-id', $(this).data('note-id'))
     setSelectedNote($(this).data());
-    ///t = setTimeout(function(){ clickedTitle() }, 1000);
+
   });
 
 };
 
 var setSelectedNote = function(data) {
-  ///clearTimeout(t);
   var id = data.noteId;
   var url = "/notes/" + id;
   $.get(url, function(data){
@@ -22,21 +21,3 @@ var setSelectedNote = function(data) {
 };
 
 window.onload = clickedTitle;
-
-
-
-
-
-
-
-
-// var interval = window.onload = setInterval(function() {
-//   console.log("loaded ::::::::::::::::::::");
-//   $("li.note-title").click(function() {
-//     window.clearInterval(interval);
-//     console.log("hit ::::::::::::::::::::::");
-//     $('textarea#selected_note').attr('data-note-id', $(this).data('note-id'))
-//     setSelectedNote($(this).data());
-//     interval;
-//   });
-// }, 1000);
