40c247af05ec0e9d55efcc7901294784630fd594
Timothy Mee <tjmee90@gmail.com>
2015-06-30 15:31:38 -0600

    refactor, styles, changed monster search back to original for now

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "/auth/twitter", as: "login"
   delete '/logout', to: "sessions#destroy"
   resources :users, only: [:create]
-  resources :notes, only: [:create, :show, :destroy, :update, :index]
+  resources :notes
 
   get 'search', to: 'search#results'
   get '/code' => redirect("https://github.com/Tmee/ruby-finder")
