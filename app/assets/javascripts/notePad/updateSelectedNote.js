900ec1dc3a024cf6655a0f5d8796d92aed2ef440
Timothy Mee <tjmee90@gmail.com>
2015-07-01 11:23:52 -0600

    added condition for id = NaN, will prompt user to click new note button.  changed variable names.

--- app/assets/javascripts/notePad/updateSelectedNote.js
+++ app/assets/javascripts/notePad/updateSelectedNote.js
@@ -1,24 +1,33 @@
 $(document).ready(function() {
   $('button#update-selected-note').click(function(e) {
     e.preventDefault();
-    updateSelectedNote();
+    checkSelectedNote();
   });
 
-  var updateSelectedNote = function () {
+  var checkSelectedNote = function () {
     var id = parseInt($('textarea#selected_note').attr('data-note-id'));
-    var url = "/notes/" + id;
-    var updatedText = {note:
-                        {
-                          title: $('input#selected_note_title').val(),
-                          body: $('textarea#selected_note').val()
-                        }
-                      }
-    $.ajax({
-      url: url,
-      method: "PUT",
-      data: updatedText
-    }).done(function(data) {
-      $('li.note-title[data-note-id=' + id +']').replaceWith("<li class='note-title' style='color:white;margin-left: 10px;' data-note-id=" + id + ">" + data.title + "</li>");
-    });
+    if (id > 0 ) {
+      updateNote(id);
+      $('textarea#selected_note').empty
+    } else {
+      alert("Looks like you don't have any notes,  Click the dropdown button to create your first note!")
+    }
   };
 });
+
+var updateNote = function(id) {
+  var url = "/notes/" + id;
+  var updatedText = {note:
+                      {
+                        title: $('input#selected_note_title').val(),
+                        body: $('textarea#selected_note').val()
+                      }
+                    }
+  $.ajax({
+    url: url,
+    method: "PUT",
+    data: updatedText
+  }).done(function(data) {
+    $('li.note-title[data-note-id=' + id +']').replaceWith("<li class='note-title' style='color:white;margin-left: 10px;' data-note-id=" + id + ">" + data.title + "</li>");
+  });
+};
