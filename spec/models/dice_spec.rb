f665586129ef527e9132f813c2efbd261a3a1b99
Timothy Mee <tjmee90@gmail.com>
2015-07-06 13:26:21 -0600

    deleted unused specs, removed vcr files.

--- spec/models/dice_spec.rb
+++ spec/models/dice_spec.rb
@@ -1,21 +1,3 @@
-require 'rails_helper'
-require 'vcr_setup'
-
 RSpec.describe Dice, :type => :model do
 
-  describe "gets correct response" do
-    it "has job elements" do
-      VCR.use_cassette "dice" do
-        response = HTTPClient.get('https://www.dice.com/jobs?l=Denver,%20CO&q=ruby')
-        expect(response.body).to include('serp-result-content')
-      end
-    end
-
-    it "has the correct number of job elements" do
-      VCR.use_cassette "dice" do
-        response = HTTPClient.get('https://www.dice.com/jobs?l=Denver,%20CO&q=ruby')
-        expect(response.body.split('serp-result-content').count).to equal(61)
-      end
-    end
-  end
 end
