e8fd807b96987b32fd14d92089156a79672a6daa
Timothy Mee <tjmee90@gmail.com>
2015-07-20 15:32:38 -0600

    removed unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :no_notes?, :most_recent_notes
+  helper_method :current_user, :no_notes?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -8,9 +8,5 @@ class ApplicationController < ActionController::Base
 
   def no_notes?
     current_user.notes.empty?
-  end
-
-  def most_recent_notes
-    current_user.notes.order("updated_at").last(3)
   end
 end
