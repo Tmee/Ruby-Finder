d72f6b031489d9bc9886418b44e8cf633c821879
Timothy Mee <tjmee90@gmail.com>
2015-07-21 08:32:36 -0600

    render 403.html for access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :no_notes?
+  rescue_from CanCan::AccessDenied do |exception|
+    render file: "#{Rails.root}/public/403", formats: [:html], status: 403, layout: false
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
