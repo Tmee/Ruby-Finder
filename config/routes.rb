f119198dd114deb1df14133b59a4c3291ed9032d
Timothy Mee <tjmee90@gmail.com>
2017-03-29 12:22:27 -0600

    routes;

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get "/auth/twitter", as: "login"
   delete '/logout', to: "sessions#destroy"
   resources :users, only: [:create]
-  resources :notes
 
   get 'search', to: 'search#results'
   get '/code' => redirect("https://github.com/Tmee/ruby-finder")
